e818f1aac7d263a138e92896ab4afa9756725fb2
mabf2608 <m.a.boni26@hotmail.com>
2024-09-24 12:32:14 +0200

    Primera Subida

---
+++ app/src/main/java/com/example/primeraapp_mabf/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.primeraapp_mabf
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.saludaButton)
+        button.setOnClickListener{
+            val name = findViewById<EditText>(R.id.nameInput).text.toString()
+            Snackbar.make(button,"Hola $name",Snackbar.LENGTH_LONG).show()
+        }
+    }
+}
